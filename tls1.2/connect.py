c651071ebb54840a1183e38242bec4ccbc57b347
Praseetha-KR <praseetha04@gmail.com>
2018-10-01 17:27:18 +0530

    Modified connect to use client inputs & formats

--- tls1.2/connect.py
+++ tls1.2/connect.py
@@ -1,109 +1,23 @@
 import socket
-import os
-import time
-
-from consts import (
-    SERVER_NAME, CLIENT_HELLO_PADDING_LEN, ALPN_PROTOCOL_1, ALPN_PROTOCOL_2,
-    EXTN_META_LEN
+from consts import SERVER_NAME
+from client_inputs import (
+    client_hello, client_key_exchange, client_change_cipher_spec,
+    client_encrypted_handshake_message
 )
-from utils import hexsplit
-from cipher_suites import (
-    CipherSuiteEnum, ECSupportedGroupEnum, SignatureHashAlgorithms
+from formats import (
+    server_hello_format, server_certificate_format, server_key_exchange_format,
+    server_hello_done_format
 )
 
-from formats.client_hello import *
-from formats.server_hello import *
 
-cipher_suites_count = len(CipherSuiteEnum)
-ec_support_group_count = len(ECSupportedGroupEnum)
-sign_hash_algo_count = len(SignatureHashAlgorithms)
-server_name_len = len(SERVER_NAME)
-alpn_1_len = len(ALPN_PROTOCOL_1)
-alpn_2_len = len(ALPN_PROTOCOL_2)
-
-client_hello = client_hello_format.build({
-    "record_content_type": 22,
-    "record_version": {
-        "major": 0x03,
-        "minor": 0x01
-    },
-    "record_length": 434,
-    "handshake_type": 1,
-    "length": 430,
-    "version": {
-        "major": 0x03,
-        "minor": 0x03
-    },
-    "random": {
-        "gmt_unix_time": int(time.time()),
-        "random_bytes": os.urandom(28)
-    },
-    "session_id_length": 0,
-    "cipher_suites_length": 2 * cipher_suites_count,
-    "cipher_suites": [hexsplit(x.value) for x in CipherSuiteEnum],
-    "compression_methods_length": 1,
-    "compression_methods": 0,
-    "extensions_length": (
-        server_name_len + 5 + EXTN_META_LEN +
-        4 + 4 +
-        2 * ec_support_group_count + 2 + EXTN_META_LEN +
-        2 * sign_hash_algo_count + 2 + 4 +
-        0 + EXTN_META_LEN +
-        alpn_1_len + alpn_2_len + 4 + EXTN_META_LEN +
-        CLIENT_HELLO_PADDING_LEN + EXTN_META_LEN
-    ),
-    "extension_server_name": {
-        "type": 0,
-        "length": server_name_len + 5,
-        "server_name_list_length": server_name_len + 3,
-        "server_name_type": 0,
-        "server_name_length": server_name_len,
-        "server_name": SERVER_NAME
-    },
-    "extension_ec_point_formats": {
-        "type": 11,
-        "length": 4,
-        "ec_point_formats_length": 3,
-        "ec_point_formats": [0, 1, 2]
-    },
-    "extension_supported_groups": {
-        "type": 10,
-        "length": 2 * ec_support_group_count + 2,
-        "supported_groups_list_length": 2 * ec_support_group_count,
-        "supported_groups": [hexsplit(x.value) for x in ECSupportedGroupEnum]
-    },
-    "extension_signature_algorithms": {
-        "type": 13,
-        "length": 2 * sign_hash_algo_count + 2,
-        "signature_hash_algorithms_length": 2 * sign_hash_algo_count,
-        "signature_hash_algorithms": [
-            hexsplit(x.value) for x in SignatureHashAlgorithms
-        ]
-    },
-    "extension_next_protocol_negotiation": {
-        "type": 13172,
-        "length": 0,
-    },
-    "extension_application_layer_protocol_negotiation": {
-        "type": 16,
-        "length": alpn_1_len + alpn_2_len + 4,
-        "alpn_extension_length": (
-            alpn_1_len + alpn_2_len + 2
-        ),
-        "alpn_string_length1": alpn_1_len,
-        "alpn_next_protocol1": ALPN_PROTOCOL_1,
-        "alpn_string_length2": alpn_2_len,
-        "alpn_next_protocol2": ALPN_PROTOCOL_2
-    },
-    "extension_padding": {
-        "type": 21,
-        "length": CLIENT_HELLO_PADDING_LEN,
-        "padding_data": b'\x00'*CLIENT_HELLO_PADDING_LEN
-    },
-})
+try:
+    ip = socket.gethostbyname(SERVER_NAME)
+    print('IP: {}'.format(ip))
+except socket.gaierror as err:
+    print('Couldn\'t resolve host IP. {}'.format(err))
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((SERVER_NAME, 443))
+s.connect((ip, 443))
 s.sendall(client_hello)
 response = b''
 while True:
@@ -113,6 +27,38 @@ while True:
     if not result:
         print('End of response.')
         break
+
+
+offset = 0
+server_hello = server_hello_format.parse(response)
+server_hello_len = server_hello.record_length + 5
+offset += server_hello_len
+server_certificate = server_certificate_format.parse(response[offset:])
+server_certificate_len = server_certificate.record_length + 5
+offset += server_certificate_len
+server_keyexchange = server_key_exchange_format.parse(response[offset:])
+server_keyexchange_len = server_keyexchange.record_length + 5
+offset += server_keyexchange_len
+server_hello_done = server_hello_done_format.parse(response[offset:])
+
+print('Server Hello: \n{}\n\n'.format(server_hello))
+print('Server Certificate: \n{}\n\n'.format(server_certificate))
+print('Server Key Exchange: \n{}\n\n'.format(server_keyexchange))
+print('Server Hello Done: \n{}\n\n'.format(server_hello_done))
+
+s.sendall(
+    client_key_exchange +
+    client_change_cipher_spec +
+    client_encrypted_handshake_message
+)
+response = b''
+while True:
+    result = s.recv(1024)
+    print('Recieved chunk of size {}...'.format(len(result)))
+    response += result
+    if not result:
+        print('End of response.')
+        break
+
 s.close()
-server_hello_response = server_hello_format.parse(response)
-print(server_hello_response)
+print(response)
